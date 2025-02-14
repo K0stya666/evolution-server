2ed941a4e0b2d0e82c41918096bc75931d677b18
Kostya666 <eliseevkonstantin306@gmail.com>
2025-02-14 18:12:04 +0300

    add Game

--- app/src/main/java/server/domain/entities/Game.java
+++ app/src/main/java/server/domain/entities/Game.java
@@ -1,4 +1,24 @@
 package server.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+import server.domain.enums.Stage;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Games")
 public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Stage stage;
+
+    @Column(name = "dice_number")
+    private Integer diceNumber;
 }
