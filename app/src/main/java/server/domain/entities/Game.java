2ebcef46059b9a98043f62825f84cd661cee270a
Kostya666 <eliseevkonstantin306@gmail.com>
2025-02-14 18:07:58 +0300

    add Stage

---
+++ app/src/main/java/server/domain/entities/Game.java
@@ -0,0 +1,4 @@
+package server.domain.entities;
+
+public class Game {
+}
---
+++ app/src/main/java/server/domain/enums/Stage.java
@@ -0,0 +1,8 @@
+package server.domain.enums;
+
+public enum Stage {
+    DEVELOPMENT,        // развитие
+    FEED_BASE,          // определение кормовой базы
+    NUTRITION,          // питание
+    EXTINCTION          // вымирание
+}
