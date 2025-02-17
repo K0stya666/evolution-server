a936f095c28c92e2c682de44b41c4ad176ffdae0
Kostya666 <eliseevkonstantin306@gmail.com>
2025-02-17 12:09:57 +0300

    fix config

--- app/src/main/java/server/domain/config/SecurityConfig.java
+++ app/src/main/java/server/domain/config/SecurityConfig.java
@@ -1,11 +1,29 @@
 package server.domain.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Настраиваем правила авторизации и прочее
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+        // Дополнительно: если нужен HTTP Basic или форма логина
+        // .httpBasic(Customizer.withDefaults())
+        // .formLogin(Customizer.withDefaults())
+        ;
+
+        return http.build();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
