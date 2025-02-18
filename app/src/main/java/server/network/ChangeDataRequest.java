3b582767448e6a1a8161afb4f1648a58cbd8cb78
Kostya666 <eliseevkonstantin306@gmail.com>
2025-02-18 20:02:21 +0300

    add ChangeDataRequest and ChangeProfileDataResponse

---
+++ app/src/main/java/server/network/ChangeDataRequest.java
@@ -0,0 +1,12 @@
+package server.network;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ChangeDataRequest(
+        // пароль или юзернейм приходит
+        @NotBlank
+        @Size(min = 5)
+        String data
+) {
+}
