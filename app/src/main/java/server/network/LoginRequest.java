d48521df8307538a86527a9675b78495af9947ee
Kostya666 <eliseevkonstantin306@gmail.com>
2025-02-18 20:19:12 +0300

    add LoginRequest

---
+++ app/src/main/java/server/network/LoginRequest.java
@@ -0,0 +1,13 @@
+package server.network;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public record LoginRequest(
+        @NotEmpty
+        String emailOrUsername,
+        @NotBlank
+        @Size(min = 5)
+        String password
+) {}
